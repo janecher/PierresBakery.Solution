e1feace5b1b95fe5238ddddab34bed4cf8313993
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-17 09:11:41 -0700

    CalculateCost method for Pastry class test failed

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -12,5 +12,19 @@ namespace PierresBakery.Tests
       Pastry pastry = new Pastry(3);
       Assert.AreEqual(typeof(Pastry), pastry.GetType());
     }
+
+    [TestMethod]
+    public void CalculateCost_CalculateTwoPastryOrderCost_Integer()
+    {
+      Pastry pastry = new Pastry(2);
+      Assert.AreEqual(4, pastry.CalculateCost());
+    }
+
+    [TestMethod]
+    public void CalculateCost_CalculateThreePastryOrderCost_Integer()
+    {
+      Pastry pastry = new Pastry(3);
+      Assert.AreEqual(5, pastry.CalculateCost());
+    }
   }
 }
