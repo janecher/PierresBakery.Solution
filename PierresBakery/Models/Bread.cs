2eecc526b76a8319c185c0b689eedf55ac3781a3
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-17 09:07:22 -0700

    create CalculateCost method, test passed

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -3,10 +3,16 @@ namespace PierresBakery.Models
   public class Bread
   {
     public int Order {get; set;}
+    public const int oneBreadCost = 5;
 
     public Bread(int order)
     {
       Order = order;
+    }
+
+    public int CalculateCost()
+    {
+      return Order / 3 * 2 * oneBreadCost + Order % 3 * oneBreadCost;
     }
   }
 }
