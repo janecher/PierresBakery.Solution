33de0ef77c0fa20aa36a374f76d53b376f2ca679
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-17 08:59:22 -0700

    ClaculateCost method test failed

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -12,5 +12,12 @@ namespace PierresBakery.Tests
       Bread bread = new Bread(3);
       Assert.AreEqual(typeof(Bread), bread.GetType());
     }
+
+    [TestMethod]
+    public void CalculateCost_CalculateBreadOrderCost_Integer()
+    {
+      Bread bread = new Bread(3);
+      Assert.AreEqual(10, bread.CalculateCost());
+    }
   }
 }
